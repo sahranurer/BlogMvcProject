2f8de2220a431bf270eb046ade30c518c2fce368
sahranurer <sahranurerr@gmail.com>
2021-12-18 23:20:47 +0300

    Migrations and Database Created

--- Entities/Concrete/Writer.cs
+++ Entities/Concrete/Writer.cs
@@ -23,7 +23,7 @@ namespace Entities.Concrete
         public string WriterPassword { get; set; }
 
         public ICollection<Heading> Headings { get; set; }
-        public ICollection<Content> Contents { get; set; }
+        //public ICollection<Content> Contents { get; set; }
 
     }
 }
