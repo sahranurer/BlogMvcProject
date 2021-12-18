1f667a971330705f2d88e68de5486c903048f83c
sahranurer <sahranurerr@gmail.com>
2021-12-18 22:28:44 +0300

    Key and String Length Attributes defined

--- Entities/Concrete/Content.cs
+++ Entities/Concrete/Content.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace Entities.Concrete
 {
     public class Content
     {
+        [Key]
         public int ContentId { get; set; }
+        [StringLength(1000)]
         public string ContentValue { get; set; }
         public DateTime ContentDate { get; set; }
 
