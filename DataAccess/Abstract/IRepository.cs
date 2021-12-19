29c3ee0f785a4c3fbd5ff841d7c6df9664e45a0a
sahranurer <sahranurerr@gmail.com>
2021-12-19 16:12:24 +0300

    Don't repeat yourself (DRY) was supported by its creation.

---
+++ DataAccess/Abstract/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IRepository<T>
+    {
+        List<T> List();
+        void Delete(T entity);
+        void Update(T entity);
+        void Insert(T entity);
+    }
+}
