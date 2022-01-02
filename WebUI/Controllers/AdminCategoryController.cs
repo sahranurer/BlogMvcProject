0c31d0ee057adfa2f309f7c906beaf0090acef47
sahranurer <sahranurerr@gmail.com>
2022-01-02 22:15:51 +0300

    Category Operations with Sidebar

--- WebUI/Controllers/AdminCategoryController.cs
+++ WebUI/Controllers/AdminCategoryController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Business.Concrete;
+using Business.ValidationRules.FluentValidation;
+using DataAccess.EntityFramework;
+using Entities.Concrete;
+using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +13,40 @@ namespace WebUI.Controllers
 {
     public class AdminCategoryController : Controller
     {
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
         // GET: AdminCategory
         public ActionResult Index()
         {
+            var results = cm.GetCategories();
+            return View(results);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category c)
+        {
+            CategoryValidator cv = new CategoryValidator();
+            ValidationResult result = cv.Validate(c);
+            if (result.IsValid)
+            {
+                cm.Add(c);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+
     }
 }
