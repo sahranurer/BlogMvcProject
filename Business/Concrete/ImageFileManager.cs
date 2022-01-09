faecb9d1fd431f6c29969529dbae46daf10a36bd
sahranurer <sahranurerr@gmail.com>
2022-01-09 19:34:46 +0300

    Image File List  Complete

---
+++ Business/Concrete/ImageFileManager.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ImageFileManager : IImageFileService
+    {
+        IImageFileDal _ımageFile;
+
+        public ImageFileManager(IImageFileDal ımageFile)
+        {
+            _ımageFile = ımageFile;
+        }
+
+      
+
+        public List<ImageFile> GetImageFiles()
+        {
+            return _ımageFile.List();
+        }
+
+        }
+    }
+
---
+++ WebUI/Controllers/GalleryController.cs
@@ -0,0 +1,22 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class GalleryController : Controller
+    {
+        ImageFileManager ımageFile = new ImageFileManager(new EfImageFileDal());
+
+        // GET: Gallery
+        public ActionResult Index()
+        {
+            var results = ımageFile.GetImageFiles();
+            return View(results);
+        }
+    }
+}
