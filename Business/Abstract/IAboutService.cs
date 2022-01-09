bca976c51ac68be113e9ee6f15a1b6b48370d94e
sahranurer <sahranurerr@gmail.com>
2022-01-09 09:55:25 +0300

    About Complete : List-Add-Partial

---
+++ Business/Abstract/IAboutService.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IAboutService
+    {
+        void Add(About about);
+        void Delete(About about);
+        void Update(About about);
+        List<About> GetAbouts();
+        About GetById(int id);
+
+
+    }
+}
---
+++ WebUI/Controllers/AboutController.cs
@@ -0,0 +1,44 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class AboutController : Controller
+    {
+
+        AboutManager am = new AboutManager(new EfAboutDal());
+        // GET: About
+        public ActionResult Index()
+        {
+            var results = am.GetAbouts();
+            return View(results);
+        }
+
+
+        [HttpGet]
+        public ActionResult AddAbout()
+        {
+            return View();
+        }
+
+        public ActionResult AddAbout(About about)
+        {
+            am.Add(about);
+            return RedirectToAction("Index");
+        }
+
+        public PartialViewResult AboutPartial()
+        {
+            return PartialView();
+        }
+
+
+
+    }
+}
