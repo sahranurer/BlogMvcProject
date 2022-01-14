98f90341f683523a96ee6de109f6e63e8b2ee41e
sahranurer <sahranurerr@gmail.com>
2022-01-14 18:52:40 +0300

    Yazar Profili Tamamlandı

--- WebUI/Controllers/WriterPanelController/WriterPanelController.cs
+++ WebUI/Controllers/WriterPanelController/WriterPanelController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Business.Concrete;
+using Business.ValidationRules.FluentValidation;
+using DataAccess.EntityFramework;
+using Entities.Concrete;
+using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +13,33 @@ namespace WebUI.Controllers.WriterPanelController
 {
     public class WriterPanelController : Controller
     {
+        WriterManager wm = new WriterManager(new EfWriterDal());
+        WriterValidator writerValidator = new WriterValidator();
         // GET: WriterPanel
-        public ActionResult WriterProfile()
+        [HttpGet]
+        public ActionResult WriterProfile(int id=0)
         {
-            return View();
+            string p = (string)Session["WriterMail"];
+            id = wm.GetWriters().Where(x => x.WriterMail == p).Select(y => y.WriterId).FirstOrDefault();
+            var writervalue = wm.GetById(id);
+            return View(writervalue);
+        }
+
+        [HttpPost]
+        public ActionResult WriterProfile(Writer w)
+        {
+           
+                wm.Update(w);
+                return RedirectToAction("AllHeading","WriterHeading");
+            ////}
+            ////else
+            ////{
+            ////    foreach (var item in result.Errors)
+            ////    {
+            ////        ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+            ////    }
+            ////}
+            //return View();
         }
     }
 }
